1085fe3c613b61b64393d98dcb688f5b6be67e50
“Luca” <luca.jones@jkweb.ch>
2023-10-30 09:00:50 +0100

    added backend springboot schöp

---
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/ShoppieMcShopFaceApplication.kt
@@ -0,0 +1,11 @@
+package ch.torata.ShoppieMcShopFace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppieMcShopFaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppieMcShopFaceApplication>(*args)
+}
---
+++ backend/src/test/kotlin/ch/torata/ShoppieMcShopFace/ShoppieMcShopFaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.torata.ShoppieMcShopFace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppieMcShopFaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
