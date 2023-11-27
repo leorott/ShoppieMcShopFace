7b46a6070d716156c2cb91e15d8c11f15e495434
“Luca” <luca.jones@jkweb.ch>
2023-11-27 08:25:33 +0100

    added entities and set up h2 db

---
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Entities/Customer.kt
@@ -0,0 +1,21 @@
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
+
+@Entity
+class Customer {
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private var id: Long? = null
+    private val firstName: String? = null
+    private val lastName: String? = null
+    private val email: String? = null
+    private val password: String? = null
+    private val address: String? = null
+    private val city: String? = null
+    private val zip: String? = null
+    private val country: String? = null
+    private val phone: String? = null
+}
---
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Entities/Product.kt
@@ -0,0 +1,40 @@
+package ch.torata.ShoppieMcShopFace.Entities
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
+
+
+@Entity
+class Product {
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private var id: Long? = null
+
+    private val name: String? = null
+
+    private val price: Double? = null
+
+    private val size: String? = null
+
+    private val description: String? = null
+
+    private val category: String? = null
+
+    private val picture: String? = null
+
+    private val stock: Int? = null
+
+    private val rating: Int? = null
+
+    private val review: String? = null
+
+    private val date: String? = null
+
+    private val time: String? = null
+
+    private val location: String? = null
+
+    private val status: String? = null
+}
