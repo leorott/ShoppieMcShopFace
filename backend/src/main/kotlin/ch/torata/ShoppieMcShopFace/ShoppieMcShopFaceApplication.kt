a14b7adcf8481c70a4d40a35a989079ba8064378
Torata <torata@Toratas-MacBook-Pro.local>
2023-12-18 08:08:51 +0100

    added logger

--- backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/ShoppieMcShopFaceApplication.kt
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/ShoppieMcShopFaceApplication.kt
@@ -2,9 +2,12 @@ package ch.torata.ShoppieMcShopFace
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.slf4j.LoggerFactory
 
 @SpringBootApplication
 class ShoppieMcShopFaceApplication
+
+private val log = LoggerFactory.getLogger(ShoppieMcShopFaceApplication::class.java)
 
 fun main(args: Array<String>) {
 	runApplication<ShoppieMcShopFaceApplication>(*args)
