d511ab8cc738577661b7fddcfface12a82b71f0e
Torata <torata@Toratas-MacBook-Pro.local>
2023-12-18 08:34:23 +0100

    added stuff to repositories

--- backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Repositories/CustomerRepository.kt
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Repositories/CustomerRepository.kt
@@ -1,2 +1,13 @@
 package ch.torata.ShoppieMcShopFace.Repositories
 
+import Customer
+import org.springframework.data.repository.CrudRepository
+
+interface CustomerRepository : CrudRepository<Customer, Long> {
+    fun findByLastName(lastName: String): List<Customer>
+    fun findByEmail(email: String): List<Customer>
+    fun findByCity(city: String): List<Customer>
+    fun findByCountry(country: String): List<Customer>
+    fun findByFirstNameAndLastName(firstName: String, lastName: String): List<Customer>
+}
+
--- backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Repositories/ProductRepository.kt
+++ backend/src/main/kotlin/ch/torata/ShoppieMcShopFace/Repositories/ProductRepository.kt
@@ -1,2 +1,10 @@
 package ch.torata.ShoppieMcShopFace.Repositories
 
+import ch.torata.ShoppieMcShopFace.Entities.Product
+import org.springframework.data.repository.CrudRepository
+
+interface ProductRepository : CrudRepository<Product, Long>{
+    fun findByStockGreaterThan(stock: Int): List<Product>
+    fun findByCategory(category: String): List<Product>
+    fun findByNameAndCategory(name: String, category: String): List<Product>
+}
